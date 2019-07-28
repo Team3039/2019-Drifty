f64a9e95913dfa31e59070f35f3220655e5c46c5
Thomas Floyd <tefloyd1215@gmail.com>
2019-07-27 23:41:11 -0500

    -Cleaned OI -Updated Readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # 2019-Drifty
 Contributors tefloyd1215@gmail.com
+
+TODO
+-Combine Translation and Rotation
+-Fix "Gap" between 0 and 360 degrees
--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -1,6 +1,5 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.buttons.Button;
 import frc.controllers.PS4Controller;
 
 public class OI {
@@ -19,34 +18,8 @@ public class OI {
 
 	public OI() {
 		//Driver Buttons
-		Button driverTriangle = driverPad.getButtonTriangle();
-		Button driverSquare = driverPad.getButtonSquare();
-		Button driverCircle = driverPad.getButtonCircle();
-		Button driverX = driverPad.getButtonX();
-		Button driverShare = driverPad.getShareButton();
-		Button driverOptions = driverPad.getOptionsButton();
-		Button driverPadButton = driverPad.getButtonPad();
-		Button driverL1 = driverPad.getL1();
-		Button driverL2 = driverPad.getL2();
-		Button driverL3 = driverPad.getL3();
-		Button driverR1 = driverPad.getR1();
-		Button driverR2 = driverPad.getR2();
-		Button driverR3 = driverPad.getR3();
 
 		//Operator Buttons
-		Button operatorTriangle = operatorPad.getButtonTriangle();
-		Button operatorSquare = operatorPad.getButtonSquare();
-		Button operatorCircle = operatorPad.getButtonCircle();
-		Button operatorX = operatorPad.getButtonX();
-		Button operatorShare = operatorPad.getShareButton();
-		Button operatorOptions = operatorPad.getOptionsButton();
-		Button operatorPadButton = operatorPad.getButtonPad();
-		Button operatorL1 = operatorPad.getL1();
-		Button operatorL2 = operatorPad.getL2();
-		Button operatorL3 = operatorPad.getL3();
-		Button operatorR1 = operatorPad.getR1();
-		Button operatorR2 = operatorPad.getR2();
-		Button operatorR3 = operatorPad.getR3();
 
 		//driver Controls
 
